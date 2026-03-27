bca9398c1a3d2d457ae9bfd78b620ddd2e81b0c3
Ishimwe Muhire Sadi <yanicksadi10@gmail.com>
2026-03-27 17:50:27 +0200

    Initial Commit to the Blog API

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express'
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json())
+
+let posts = [
+  {
+    id: 1,
+    title: "First Post",
+    content: "This is my first blog post"
+  },
+  {
+    id: 2,
+    title:"Second Post",
+    content: "Learning Node is Fun!"
+  }
+];
+
+app.get("/", (req, res)=>{
+  res.send({message:"Welcome to Blog Api"})
+})
+
+// here we can get all posts through the posts api
+
+app.get("/api/posts", (req, res)=> {
+  res.json(posts);
+})
+
+//here we can get one post by their ID
+
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find(p => p.id === id);
+
+  if(!post){
+    return res.status(404).send("Post not found");
+  }
+
+  res.json(post);
+
+})
+
+app.listen(PORT, () => {
+  console.log(`server is running on PORT: ${PORT}`)
+})
